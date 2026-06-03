8eea89d7d31cfe0b9672233e2002b76442155ecd
alimont01 <hello@alimontgomery.com>
2026-06-03 09:40:21 +0100

    404

--- 404.php
+++ 404.php
@@ -10,14 +10,31 @@ get_header();
 
   <main class="site-main flex-grow-1 align-content-center">
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="container py-3 py-md-5 mt-5 z-top fadein-content">
-          <div class="row">
-            <div class="col-12">
-              
-              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button">Return to home</a>
-            </div>
+
+    <header class="container-fluid hero cream-bg recipe-bg">
+      <div class="container pb-5">
+        <div class="row justify-content-center">
+          <div class="col-lg-7 text-center">
+            <img class="w-100" src="<?php echo get_template_directory_uri(); ?>/assets/img/broken-plate.svg" alt="Nyssa Hartwood Error 404 broken plate">
+            <h1 class="mt-0">
+              Oops! This isn't what we ordered.
+            </h1>
           </div>
         </div>
+      </div>
+    </header>
+
+    <div class="container py-3 py-md-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-7 text-center">
+          <h2>404 Error</h2>
+          <p>The page you're looking for can't be found. It may have been moved, updated, or removed.</p>
+          <p>Please return <a href="<?php echo esc_url( home_url( '/' ) ); ?>">home</a>, shop our <a href="/shop/">books</a>, or explore our collection of <a href="/recipes/">recipes</a>.</p>
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button">Return to home</a>
+        </div>
+      </div>
+    </div>
+
     </article>
   </main>
 
