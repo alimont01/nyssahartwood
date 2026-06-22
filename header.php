579a3a89748accba4455a3167d6463a32399e9cf
alimont01 <hello@alimontgomery.com>
2026-06-22 12:13:24 +0100

    g-tags

--- header.php
+++ header.php
@@ -7,9 +7,18 @@
 ?><!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LT8DC9SBQ1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LT8DC9SBQ1');
+    </script>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
